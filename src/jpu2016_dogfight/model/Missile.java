41d18b46400fb3fbe4512e196840fed366b43bc6
flori <flori@DESKTOP-F58S82D>
2018-04-23 17:13:40 +0200

    Phase 3

--- src/jpu2016_dogfight/model/Missile.java
+++ src/jpu2016_dogfight/model/Missile.java
@@ -1,5 +1,91 @@
 package jpu2016_dogfight.model;
 
 public class Missile {
+	
+	private static int SPEED = 4;
+	private static int WIDTH = 30;
+	private static int HEIGHT = 10;
+	private static int MAX_DISTANCE_TRAVELED = 1400;
+	private static String IMAGE = "missile";
+	private int distanceTraveled = 0;
+	
+	public Missile(Direction direction, Dimension dimension) {
+		
+		/*
+		 * 
+		 */
+	}
+	
+	public static int getWidthWithADirection(Direction direction) {
+		return 1;
+	}
+	
+	public static int getHeightWithADirection(Direction direction) {
+		return 1;
+	}
+	
+	public void move() {
+		
+		/*
+		 * 
+		 */
+	}
+	
+	public boolean isWeapon() {
+		return true;
+	}
+	
+	public static int getSPEED() {
+		return SPEED;
+	}
+	
+	public static void setSPEED(int sPEED) {
+		SPEED = sPEED;
+	}
+
+	public static int getWIDTH() {
+		return WIDTH;
+	}
+
+	public static void setWIDTH(int wIDTH) {
+		WIDTH = wIDTH;
+	}
+
+	public static int getHEIGHT() {
+		return HEIGHT;
+	}
+
+	public static void setHEIGHT(int hEIGHT) {
+		HEIGHT = hEIGHT;
+	}
+
+	public static int getMAX_DISTANCE_TRAVELED() {
+		return MAX_DISTANCE_TRAVELED;
+	}
+
+	public static void setMAX_DISTANCE_TRAVELED(int mAX_DISTANCE_TRAVELED) {
+		MAX_DISTANCE_TRAVELED = mAX_DISTANCE_TRAVELED;
+	}
+
+	public static String getIMAGE() {
+		return IMAGE;
+	}
+
+	public static void setIMAGE(String iMAGE) {
+		IMAGE = iMAGE;
+	}
+
+	public int getDistanceTraveled() {
+		return distanceTraveled;
+	}
+
+	public void setDistanceTraveled(int distanceTraveled) {
+		this.distanceTraveled = distanceTraveled;
+	}
+	
+	
+	
+	
+	
 
 }
--- src/jpu2016_dogfight/model/Position.java
+++ src/jpu2016_dogfight/model/Position.java
@@ -1,5 +1,56 @@
 package jpu2016_dogfight.model;
 
 public class Position {
+	
+	private double x;
+	private double y;
+	private double  maxX;
+	private double maxY;
+	
+	
+	public Position(double x, double y, double maxX, double maxY) {
+		/*
+		 * 
+		 */
+	}
+	
+	public Position(Position position) {
+		/*
+		 * 
+		 */
+	}
+	
+	
+	public double getX() {
+		return x;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	protected void setMaxX(double maxX) {
+		/*
+		 * 
+		 */
+	}
+	
+	protected void setMaxY(double maxY) {
+		/*
+		 * 
+		 */
+	}
+	
+	
+	
+	
 
 }
