ec6fe5fa29be04d3aa8e51385b43ab19df1019a7
Hugo-PC <Hugo-PC@HUGO-PC>
2018-04-23 16:52:24 +0200

    Phase 3

---
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,29 @@
+package jpu2016.dogfight.controller;
+
+public class DogfightController {
+	private int TIME_SLEEP=30;
+
+	public DogfightController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void play() {
+		
+	}
+
+	public void gameLoop() {
+		
+	}
+	
+	private void lauchMissile(int player) {
+		
+	}
+
+	public void setViewSystem() {
+		
+	}
+	
+	public UserOrder orderPerform() {
+		return new UserOrder();
+	}
+}
---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,18 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder {
+	private int player;
+
+	public UserOrder() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getPlayer() {
+		return 1;
+	}
+	
+	public Order getOrder() {
+		return new Order;
+		
+	}
+}
