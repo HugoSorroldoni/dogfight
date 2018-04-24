446403793a588d352faba4517c74ca8b049c2bb5
flori <flori@DESKTOP-F58S82D>
2018-04-24 10:06:35 +0200

    Phase 3

--- src/jpu2016/dogfight/model/IDogfightModel.java
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface IDogfightModel {
 	
 	public IArea getArea();
-	public void buildArena(Dimension dimension);
+	public void buildArea(Dimension dimension);
 	public void addMobile(IMobile Mobile);
 	public void removeMobile(IMobile Mobile);
 	public ArrayList<IMobile> getMobiles();
