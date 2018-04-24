78d83f304ee1b11acc18843ae171acbe3a18d54a
flori <flori@DESKTOP-F58S82D>
2018-04-24 10:04:50 +0200

    Phase 3

--- src/jpu2016/dogfight/model/Position.java
+++ src/jpu2016/dogfight/model/Position.java
@@ -2,4 +2,50 @@ package jpu2016.dogfight.model;
 
 public class Position {
 
+	private double x;
+	private double y;
+	private double maxX;
+	private double maxY;
+	
+	
+	public Position(double x, double y, double maxX, double maxY) {
+		/*
+		 * 
+		 */
+	}
+	
+	public Position(Position position) {
+		/*
+		 * 
+		 */
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public void setX(double x) {
+		x = this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+		
+	}
+	
+	public void setY(double y) {
+		y = this.y;
+	}
+	
+	protected void setMaxX(double maxX) {
+		maxX = this.maxX;
+	}
+	
+	protected void setMaxY(double maxY) {
+		
+		maxY = this.maxY;
+	}
+	
+	
+	
 }
