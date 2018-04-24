deedb9dffc82c7e59e3c13000b3743883482c041
flori <flori@DESKTOP-F58S82D>
2018-04-24 09:11:48 +0200

    Phase 3

--- src/jpu2016/dogfight/model/DogfightModel.java
+++ src/jpu2016/dogfight/model/DogfightModel.java
@@ -1,7 +1,53 @@
 package jpu2016.dogfight.model;
+import java.util.ArrayList;
 
 public class DogfightModel {
 	
+	public DogfightModel() {
+		/*
+		 * 
+		 */
+	}
 	
+	public IArea getArea() {
+		/* a modif */
+		return null;
+	}
+	
+	public void buildArea(Dimension dimension) {
+		/*
+		 * 
+		 */
+	}
+	
+	public void addMobile(IMobile Mobile) {
+		/*
+		 * 
+		 */
+	}
+	
+	public void removeMobile(IMobile Mobile) {
+		/*
+		 * 
+		 */
+	}
 
+	public ArrayList<IMobile> getMobiles(){
+		/* a modif */
+		return null;
+		
+	}
+	
+	public IMobile getMobileByPlayer(int player) {
+		/* a modif */
+		return null;
+		
+	}
+	
+	public void setMobilesHavesMoved() {
+		/*
+		 * 
+		 */
+		
+	}
 }
