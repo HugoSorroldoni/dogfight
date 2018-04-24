a766cc89a861cabe0f8759b79c2aeaf440f6315b
soria <soria@MSI>
2018-04-24 09:07:50 +0200

    phase 3

---
+++ src/src/jpu2016/dogfight/view/GraphicsBuilder.java
@@ -0,0 +1,22 @@
+package jpu2016.dogfight.view;
+
+import java.awt.Graphics;
+import java.awt.image.ImageObserver;
+
+import jpu2016_dogfight.model.IDogfightModel;
+
+public class GraphicsBuilder {
+
+	public GraphicsBuilder(IDogfightModel dogfightModel) {
+		
+	}
+	public void applyModelToGraphic(Graphics graphics, ImageObserver observer) {
+		
+	}
+	private void buildEmptySky() {
+		
+	}
+	private void drawMobile(Dogfight Model.jpu2016.dogfight.model.IMobile mobile, Graphics graphics, ImageObserver observer) {
+		
+	}
+}
