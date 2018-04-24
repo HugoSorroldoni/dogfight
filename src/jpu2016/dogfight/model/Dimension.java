5153110c6450c8224892bd0f54b04a8f97882070
flori <flori@DESKTOP-F58S82D>
2018-04-24 09:01:54 +0200

    Phase 3

--- src/jpu2016/dogfight/model/Dimension.java
+++ src/jpu2016/dogfight/model/Dimension.java
@@ -2,4 +2,38 @@ package jpu2016.dogfight.model;
 
 public class Dimension {
 
+	private int width;
+	private int height;
+	
+	public Dimension(int width, int height) {
+		/*
+		 * 
+		 */
+	}
+	
+	public Dimension(Dimension dimension) {
+		/*
+		 * 
+		 */
+	}
+	
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public void setWidth(int width) {
+		/*
+		 * 
+		 */
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	
+	
+	
+	
+	
+	
 }
