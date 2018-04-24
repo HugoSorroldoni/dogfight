3c4104c7604ea5da73702f03a8caf4a8d241dc61
flori <flori@DESKTOP-F58S82D>
2018-04-24 08:58:06 +0200

    Phase 3

--- src/jpu2016/dogfight/model/Cloud.java
+++ src/jpu2016/dogfight/model/Cloud.java
@@ -7,18 +7,42 @@ public class Cloud {
 	private static int HEIGHT = 150;
 	private static String IMAGE = "cloud";
 	
+	
+	public Cloud(Direction direction, Dimension dimension) {
+		
+		
+	}
+	
+	
+	
 	public static int getSPEED() {
 		return SPEED;
 	}
 	public static void setSPEED(int sPEED) {
 		SPEED = sPEED;
 	}
-	
-	
-	public static void tg() {
-		
-		
+	public static int getWIDTH() {
+		return WIDTH;
 	}
+	public static void setWIDTH(int wIDTH) {
+		WIDTH = wIDTH;
+	}
+	public static int getHEIGHT() {
+		return HEIGHT;
+	}
+	public static void setHEIGHT(int hEIGHT) {
+		HEIGHT = hEIGHT;
+	}
+	public static String getIMAGE() {
+		return IMAGE;
+	}
+	public static void setIMAGE(String iMAGE) {
+		IMAGE = iMAGE;
+	}
+	
+	
+	
+	
 	
 	
 	
