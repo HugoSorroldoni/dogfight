830424ad74c63585f974fc67baa264f07fed9d6c
flori <flori@DESKTOP-F58S82D>
2018-04-24 09:59:36 +0200

    Phase 3

--- src/jpu2016/dogfight/model/Plane.java
+++ src/jpu2016/dogfight/model/Plane.java
@@ -2,7 +2,56 @@ package jpu2016.dogfight.model;
 
 public class Plane {
 
+	private static int SPEED = 2;
+	private static int WIDTH = 100;
+	private static int HEIGHT = 30;
+	private int player;
 	
+	public Plane(int player, Direction direction, Position position, String image) {
+		/*
+		 * 
+		 */
+	}
+	
+	public boolean isPlayer(int player) {
+		return true;
+	}
+	
+	public boolean hit() {
+		return true;
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+	}
+
+	public static int getHEIGHT() {
+		return HEIGHT;
+	}
+
+	public static void setHEIGHT(int hEIGHT) {
+		HEIGHT = hEIGHT;
+	}
+
+	public static int getWIDTH() {
+		return WIDTH;
+	}
+
+	public static void setWIDTH(int wIDTH) {
+		WIDTH = wIDTH;
+	}
+
+	public static int getSPEED() {
+		return SPEED;
+	}
+
+	public static void setSPEED(int sPEED) {
+		SPEED = sPEED;
+	}
 	
 	
 	
