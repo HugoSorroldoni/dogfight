5e2c351be01c73594d952a4d848eb640ec8acb21
soria <soria@MSI>
2018-04-24 09:26:38 +0200

    Phase 3

---
+++ src/jpu2016/gameframe/GameFrame.java
@@ -0,0 +1,15 @@
+package jpu2016.gameframe;
+import java.awt.event.KeyListener;
+import java.util.Observable;
+
+import javax.swing.JFrame; 
+
+public class GameFrame {
+	public GameFrame(String title, View.jpu2016.gameframe.IEventPerformer performer, View.jpu2016.gameframe.IGraphicsBuilder graphicBuilder, Observable observable) {
+		
+	}
+	
+
+	
+	
+}
